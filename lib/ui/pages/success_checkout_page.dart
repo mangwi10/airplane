376fc5fdd25a91306db9a9be0d2c0959a8a37ffb
mangwi10 <mankwiyana@gmail.com>
2024-02-12 16:37:32 +0800

    menambahkan beberapa page

---
+++ lib/ui/pages/success_checkout_page.dart
@@ -0,0 +1,58 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class SuccessCheckoutPage extends StatelessWidget {
+  const SuccessCheckoutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBgColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 150,
+              margin: EdgeInsets.only(bottom: 80),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/image_success.png'),
+                ),
+              ),
+            ),
+            Text(
+              'Well Book 😊',
+              style: blackTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: semiBold,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Are you ready to explore the new \nworld of experiences?',
+              style: greyTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: light,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            customButton(
+              title: 'My Booking',
+              onPressed: () {
+                Navigator.pushNamedAndRemoveUntil(
+                    context, '/main', (route) => false);
+              },
+              width: 220,
+              margin: EdgeInsets.only(top: 50),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/pages/wallet_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class WalletPage extends StatelessWidget {
+  const WalletPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Wallet Page'),
+    );
+  }
+}
